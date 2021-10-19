9b8048d6f867f54405f2bbb050a2d78f3cc002d7
sudarshan naidu <sudarshan.naidu@sts.in>
2021-10-19 18:57:58 +0530

    code updated

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -20,9 +20,9 @@ import com.qa.util.WebEventListener;
 public class TestBase {
 	public static WebDriver driver;
 	//browserstack setup
-	public static final String USERNAME = "sudarshan30";
-	public static final String AUTOMATE_KEY = "fZAL6pfg3KtkyYCuw1ye";
-	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+	//public static final String USERNAME = "sudarshan30";
+	//public static final String AUTOMATE_KEY = "fZAL6pfg3KtkyYCuw1ye";
+	//public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 	
 	//selenium grid congig
 	//public static DesiredCapabilities dr=null;
@@ -58,28 +58,28 @@ public static void insilization() throws MalformedURLException{
 	//dr.setPlatform(Platform.WINDOWS);
 	
 	//browser stack
-	DesiredCapabilities caps=new DesiredCapabilities();
-	caps.setCapability("browser", "chrome");
-	caps.setCapability("browser_version", "94.0");
-	caps.setCapability("os", "Windows");
-	caps.setCapability("os_version", "10");
-	caps.setCapability("build", "browserstack-build-1");
+	//DesiredCapabilities caps=new DesiredCapabilities();
+	//caps.setCapability("browser", "chrome");
+	//caps.setCapability("browser_version", "94.0");
+	//caps.setCapability("os", "Windows");
+	//caps.setCapability("os_version", "10");
+	//caps.setCapability("build", "browserstack-build-1");
 	
 	//caps.setCapability("name", "Thread 1");
 	
 	
 	if(browserName.equals("chrome")){
-		//System.setProperty("webdriver.chrome.driver","D:/chromedriver/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","D:/chromedriver/chromedriver.exe");
 
 		//selenium grid setup
 		//driver =new RemoteWebDriver(new URL("http://192.168.1.4:4444/wd/hub"),dr);
 		//driver =new RemoteWebDriver(new URL(URL),caps);
-		driver =new RemoteWebDriver(new java.net.URL(URL),caps);
+		//driver =new RemoteWebDriver(new java.net.URL(URL),caps);
 
-		//driver=new ChromeDriver();
+		driver=new ChromeDriver();
 		
 	}else if(browserName.equals("FF")){
-		//System.setProperty("webdriver.gecko.driver", "D:/geckodriver-v0.30.0-win64/geckodriver.exe");	
+		System.setProperty("webdriver.gecko.driver", "D:/geckodriver-v0.30.0-win64/geckodriver.exe");	
 		driver = new FirefoxDriver(); 
 	}
 	e_driver = new EventFiringWebDriver(driver);
